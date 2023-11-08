181d9cafcbaa136809456cc1b3d7948e3ad5caec
Yves Bonjour <yves.bonjour@rethinc.ch>
2023-11-08 11:30:08 +0100

    Revert "Stores counter in bundle"
    This reverts commit 4bf80fe0a76876e2259fb70bfe5114c58f3477d9.

--- app/src/main/java/ch/bfh/cas/mad/counter/MainActivity.kt
+++ app/src/main/java/ch/bfh/cas/mad/counter/MainActivity.kt
@@ -2,15 +2,13 @@ package ch.bfh.cas.mad.counter
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val counterKey = "ch.bfh.cas.counter"
-    }
 
     private lateinit var textViewCounter: TextView
     private lateinit var buttonIncrease: Button
@@ -21,16 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        savedInstanceState?.getInt(counterKey)?.let { counter = it }
         textViewCounter = findViewById(R.id.textView_counter)
         buttonIncrease = findViewById(R.id.button_increase)
         buttonAlert = findViewById(R.id.button_alert)
         refreshCounter()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(counterKey, counter)
     }
 
     override fun onResume() {
